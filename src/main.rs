7a05e18a1b8a93f50a4be8f5b978d602aa794d6b
John Blackburn <jackblackburnn@icloud.com>
2025-02-05 16:46:17 -0500

    Test printing model

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod perceptron;
-use perceptron::Data;
+use perceptron::{Data, Model};
 
 fn main() {
     let data = Data::new("train.dat");
-
     data.print();
+
+    let model = Model::new(data);
+    model.print();
 }
