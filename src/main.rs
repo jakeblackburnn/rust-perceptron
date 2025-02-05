fec7f76980c2385d8341fade4e619e434ab158ea
John Blackburn <jackblackburnn@icloud.com>
2025-02-05 16:21:37 -0500

    Add Data struct w/ print method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod perceptron;
+use perceptron::Data;
 
 fn main() {
     perceptron::hello();
+
+    let data = Data::new();
+
+    data.print();
 }
--- src/perceptron.rs
+++ src/perceptron.rs
@@ -1,3 +1,43 @@
 pub fn hello() {
     println!("Hello, world!");
 }
+
+
+    // data struct
+pub struct Data {
+    elements: Vec<Vec<f64>>,
+    targets: Vec<i32>,
+    rows: usize,
+    columns: usize,
+}
+
+impl Data {
+
+    pub fn new() -> Self {
+
+        Data {
+            elements: vec![
+                vec![1.0, 2.0, 3.0],
+                vec![4.0, 5.0, 6.0],
+            ],
+            targets: vec![0, 1],
+            rows: 2,
+            columns: 3,
+        }
+        
+    }
+
+    pub fn print(&self) {
+        println!("========== DISPLAYING DATA ==========\n");
+
+        let mut i = 0;
+        for feature_vector in &self.elements {
+            print!("{:?}", feature_vector);
+            println!("  {}", self.targets[i]);
+            i += 1;
+        }
+        println!("\n");
+    }
+
+}
+
